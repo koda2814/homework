62adada4eee8276f5b5c7fd875af7abd302b5a73
Zeding_Stuff <zeding.420@gmail.com>
2023-05-30 22:16:48 +1000

    hw7 is done

---
+++ hw7/hw2.py
@@ -0,0 +1,28 @@
+"""
+Write a context manager, that suppresses passed exception.
+Do it both ways: as a class and as a generator.
+
+>>> with supressor(IndexError):
+...    [][2]
+
+"""
+from contextlib import contextmanager
+
+class Suppressor:
+    def __init__(self, exception):
+        self.exception = exception
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        return isinstance(exc_value, self.exception)
+
+
+
+@contextmanager
+def suppressor(exception):
+    try:
+        yield
+    except exception:
+        pass
